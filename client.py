92f7a1cd6325976490ffb949da03b0d5105f94fd
athulnair02 <42418601+athulnair02@users.noreply.github.com>
2023-03-14 22:00:13 -0500

    Create client.py

---
+++ client.py
@@ -0,0 +1,40 @@
+# Imports
+from ctypes.wintypes import INT
+import socket
+import subprocess
+
+# Setting Up IP/Sockets
+REMOTE_HOST = '10.0.2.5'
+REMOTE_PORT = 4444
+client = socket.socket()
+
+# Initializing Connection
+print("[-] Connection Initiating...")
+client.connect((REMOTE_HOST, REMOTE_PORT))
+print("[-] Connection initiated!")
+
+
+password = input('Enter Password: ')
+client.send(password.encode())
+
+# Runtime Loop
+while True:
+        command = input('Enter Command: ')
+        while command == '':
+                command = input('Enter Command: ')
+
+        command = command.encode()
+
+        client.send(command)
+
+        if command == b'exit':
+                break
+
+        output = client.recv(1024)
+        output = output.decode()
+        if output == 'no stdout':
+                print()
+        else:
+                print(output)
+
+client.close()
